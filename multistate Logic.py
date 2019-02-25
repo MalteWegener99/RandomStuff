b09f467a18cd54287c2aa2bf86c3f85743396738
MalteWegener <malte.wegener99@gmx.de>
2019-02-25 21:21:27 +0100

    Added Text

--- multistate Logic.py
+++ multistate Logic.py
@@ -2,16 +2,42 @@ import itertools
 import numpy as np
 
 def make_table(operators):
-    numbers = [0b000,0b001,0b010,0b011,0b100,0b101,0b110,0b111]
-    all_combs = list(itertools.product(range(0,8), repeat=2))
+    size = 3
+    numbers = [0b0000,0b0001,0b0010,0b0011,0b0100,0b0101,0b0110,0b0111]#,0b1000,0b1001,0b1010,0b1011,0b1100,0b1101,0b1110,0b1111]
+    all_combs = list(itertools.product(range(0,2**size), repeat=2))
 
     collector = []
+    faulty_add = 0
+    faulty_sub = 0
+    faulty_mul = 0
+    faulty_div = 0
 
     for comb in all_combs:
         collector2 = [comb[0], comb[1]]
+
+        x = comb[0]
+        y = comb[1]
+
+        if x + y > 7:
+            faulty_add += 1
+        if x - y < 0:
+            faulty_sub += 1
+        if x*y > 7:
+            faulty_mul += 1
+        if y == 0:
+            faulty_div += 1
+        elif x%y != 0:
+            faulty_div += 1
+
         for op in operators:
             collector2.append(op(comb))
         collector.append(collector2)
+
+    print(faulty_add)
+    print(faulty_sub)
+    print(faulty_mul/(2**(2*size)))
+    print(faulty_div)
+    print((faulty_add+faulty_div+faulty_mul+faulty_sub)/(8*2**(2*size)))
 
     return collector
 
@@ -57,4 +83,4 @@ def tablemaker(table):
     print(r'\end{center}')
 
 table = make_table([AND, OR, XOR, NOT, ADD, SUB, MULT, DIV])
-tablemaker(table)
+#tablemaker(table)
